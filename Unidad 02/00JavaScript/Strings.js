38c07de500edc543bd5e58255239ec20b0c9e415
Alexander <rodolfo.gallo@unl.edu.ec>
2026-06-12 13:07:13 -0500

    feat: Learning Javascript for noobscis

---
+++ Unidad 02/00JavaScript/Strings.js
@@ -0,0 +1,11 @@
+let string = 'Hola, mundo'
+let string2 = "Hola, mundo"
+let string3 = `${string} feliz`
+let string4 = string + ' ' + 'Feliz'
+let frase = 'JavaScript es genial'
+
+console.log(string3)
+console.log(frase.length)
+console.log(frase.toLocaleUpperCase())
+console.log(frase.toLowerCase())
+console.log(frase.substring(0,10))
---
+++ Unidad 02/00JavaScript/tipos-datos.js
@@ -0,0 +1,34 @@
+
+// PRIMITIVOS 
+let nombre = 'Alexander'
+
+//BOOLEANOS 
+let esMayorDeEdad = true
+
+//NULL
+let NoHayValor = null;
+
+//UNDEFINED
+let noDefinido = undefined
+
+//SIMBOLOS VALOR UNCO
+let simboloUnico = Sumbol('único')
+
+//DATOS GIGANTES BIGINT 
+let numeroGrande = 2n
+
+//COMPLETOS 
+//object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Modelo S',
+}
+
+//ARRAYS
+let frutas = ['Manzana', 'Banana', 'Pera']
+console.log(frutas)
+
+//FUNCTIONS
+function saludar(){}
+
+
